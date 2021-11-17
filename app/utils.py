0a2c0d2f0625a318c53564f0abe4ccf3703ed8e3
qiokis <70791542+qiokis@users.noreply.github.com>
2021-11-17 19:02:46 +0300

    feat: folders_setup - setup of dirs with pictures for working uploading functions feat: allowed_file - check extension of file according to config 'ALLOWED_EXTENSIONS' feat: generate_filename - generate filename and filepath for save files

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,20 @@
 from app import db, app
 from app.models import User
+import config
+from flask_login import current_user
+from flask import redirect, url_for
+from hashlib import md5
+import os
+
+
+def folders_setup():
+    folders = ['app\\static', 'app\\static\\images']
+
+    folders.extend([f'{folders[1]}\\category',
+                    f'{folders[1]}\\product'])
+    for folder in folders:
+        if not os.path.exists(folder):
+            os.mkdir(folder)
 
 
 def check_user_existence(username='', email=''):
@@ -11,6 +26,16 @@ def check_user_existence(username='', email=''):
         return True if u else False
 
 
+def allowed_file(filename):
+    return '.' in filename and filename.split('.')[-1] in app.config['ALLOWED_EXTENSIONS']
+
+
+def generate_filename(filename, file_id, addition):
+    ext = filename.split('.')[-1]
+    filename = md5(f'{file_id}-{filename}'.encode()).hexdigest()
+    filename += f'.{ext}'
+    return filename, os.path.join(app.config['UPLOAD_FOLDER']+f'\\{addition}', filename)
+
 if __name__ == '__main__':
-    test = 'qioki'
-    print(check_user_existence(email='qioki@example.com'))
+    test = 'qioki.txt'
+    print(generate_filename(test))
