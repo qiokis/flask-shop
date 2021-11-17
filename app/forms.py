469d53fed6b1f42903de5c3746354c4005677cd8
qiokis <70791542+qiokis@users.noreply.github.com>
2021-11-17 18:49:28 +0300

    fix: replace StringField with PasswordField for password fix: replace StringField with FileField for picture

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,12 @@
-from wtforms import StringField, TextAreaField, IntegerField, FloatField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms import StringField, TextAreaField, IntegerField, FloatField, FileField, PasswordField
+from wtforms.validators import DataRequired, EqualTo, regexp
 from flask_wtf import FlaskForm
+import re
 
 
 class UserLoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
 
 
 class UserRegistrationForm(FlaskForm):
@@ -13,18 +14,18 @@ class UserRegistrationForm(FlaskForm):
     surname = StringField('surname', validators=[DataRequired()])
     username = StringField('username', validators=[DataRequired()])
     email = StringField('email', validators=[DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
-    password2 = StringField('password2', validators=[DataRequired(), EqualTo('password')])
+    password = PasswordField('password', validators=[DataRequired()])
+    password2 = PasswordField('password2', validators=[DataRequired(), EqualTo('password')])
 
 
 class CategoryForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
-    picture = StringField('picture', validators=[DataRequired()])
+    picture = FileField('picture', validators=[DataRequired()])
 
 
 class ProductForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
     description = TextAreaField('description', validators=[DataRequired()])
-    picture = StringField('picture', validators=[DataRequired()])
+    picture = FileField('picture', validators=[DataRequired()])
     price = FloatField('price', validators=[DataRequired()])
     mark = FloatField('mark', validators=[DataRequired()])
