5879cdafae14bacd95d7e313e25c70e45e64a54f
qiokis <70791542+qiokis@users.noreply.github.com>
2021-11-17 18:47:55 +0300

    feat: add options for uploading files

--- config.py
+++ config.py
@@ -10,6 +10,8 @@ class BaseConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(app_dir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = 'app\\static\\images'
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 
 class DevelopmentConfig(BaseConfig):
