a1b4fbb21ae948b62cfc230985872b52e5e0a287
qiokis <70791542+qiokis@users.noreply.github.com>
2021-11-18 13:50:17 +0300

    refactor: unlogged users now redirect to login page

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,6 @@ app.config.from_object(os.environ.get('FLASK_ENV') or 'config.DevelopmentConfig'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager(app)
-login_manager.login_view = 'index'
+login_manager.login_view = 'login'
 
 from . import views, models
