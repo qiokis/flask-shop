c72071b1ffb774e6680a8843ef61a1afcb5066b9
qiokis <70791542+qiokis@users.noreply.github.com>
2021-11-18 20:36:58 +0300

    refactor: add name and picture attributes to post model

--- app/models.py
+++ app/models.py
@@ -36,6 +36,11 @@ class User(db.Model, UserMixin):
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    picture = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'<Post>-Name:{self.name}'
 
 
 class Category(db.Model):
